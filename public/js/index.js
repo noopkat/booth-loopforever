a066e98b149ab1eb23d90f97be9bdf7ac7b6dfb2
noopkat <suz.hinton@gmail.com>
2015-02-19 00:28:20 -0500

    spaces

--- public/js/index.js
+++ public/js/index.js
@@ -1,56 +1,56 @@
 (function() {
 
-	var session = {
+  var session = {
     audio: false,
     video: true
-	};
+  };
 
-	var mediaElement = document.getElementById('yesss');
-	var capture = document.getElementById('capture');
-	var video = document.getElementById('vid');
-	var recordRTC, options;
-	 
-	navigator.getUserMedia(session, function(mediaStream) {
+  var mediaElement = document.getElementById('yesss');
+  var capture = document.getElementById('capture');
+  var video = document.getElementById('vid');
+  var recordRTC, options;
+   
+  navigator.getUserMedia(session, function(mediaStream) {
 
-		video.src = window.URL.createObjectURL(mediaStream);
-		video.play();
+    video.src = window.URL.createObjectURL(mediaStream);
+    video.play();
 
-	 	options = {
-		   type: 'gif',
-		   frameRate: 100,
-		   quality: 10
-		};
+    options = {
+       type: 'gif',
+       frameRate: 100,
+       quality: 10
+    };
 
-		recordRTC = RecordRTC(mediaStream, options);
+    recordRTC = RecordRTC(mediaStream, options);
 
-		capture.addEventListener('click', function(e) {
+    capture.addEventListener('click', function(e) {
 
-			e.preventDefault;
+      e.preventDefault();
 
-			e.target.style.opacity = 0;
-			video.style.display = 'inline-block';
-			mediaElement.style.display = 'none';
+      e.target.style.opacity = 0;
+      video.style.display = 'inline-block';
+      mediaElement.style.display = 'none';
 
-			recordRTC.startRecording();
+      recordRTC.startRecording();
 
-			setTimeout(stopAndShow, 2000);
+      setTimeout(stopAndShow, 2000);
 
-			function stopAndShow() {
+      function stopAndShow() {
 
-				recordRTC.stopRecording(function(gifURL) {
+        recordRTC.stopRecording(function(gifURL) {
 
-			   		mediaElement.src = gifURL;
+            mediaElement.src = gifURL;
 
-			   		video.style.display = 'none';
-			   		e.target.style.opacity = 1;
-			   		mediaElement.style.display = 'inline-block';
+            mediaElement.style.display = 'inline-block';
+            e.target.style.opacity = 1;
+            video.style.display = 'none';
 
-					});
+          });
 
-				}
+        }
 
-			});
+      });
 
-			}, function() { /* do error handling in here eventually */ });
+      }, function() { /* do error handling in here eventually */ });
 
 })();
