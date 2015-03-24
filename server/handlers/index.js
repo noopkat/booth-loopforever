19f166a5cd34fbc5466b897d708829da4b38ccac
Suz Hinton <noopkat@users.noreply.github.com>
2015-03-24 00:50:38 -0400

    there is a reason why we can't have nice things

--- server/handlers/index.js
+++ server/handlers/index.js
@@ -1,3 +1,3 @@
 module.exports = function(request, reply) {
-  reply.view('index', {stuff: 'right click -> save as when you're happy with your gif'});
+  reply.view('index', {stuff: 'right click -> save as when you\'re happy with your gif'});
 };
